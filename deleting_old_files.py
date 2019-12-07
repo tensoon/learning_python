97d7df235acc5364c4801dcd81475aa8d073ea13
tensoon <son1k@protonmail.com>
2019-12-07 12:45:31 +0000

    file delete and a file copy added

---
+++ deleting_old_files.py
@@ -0,0 +1,16 @@
+import os, time, sys
+
+folder_path = r"C:\Users\nbalinsk\Downloads"
+# file_ends_with = ".txt" < -- Specific file type to be deleted
+days = 1
+
+now = time.time()
+only_files = []
+
+for file in os.listdir(folder_path):
+    file_full_path = os.path.join(folder_path,file)
+    if os.path.isfile(file_full_path): #and file.endswith(file_ends_with):
+        #Delete files older than x days
+        if os.stat(file_full_path).st_mtime < now - days * 86400: 
+             os.remove(file_full_path)
+             print ("File Removed: " , file_full_path)
---
+++ smart_copy.py
@@ -0,0 +1,31 @@
+import os
+import re
+import shutil
+
+''' 
+The below code isn't pretty but it does the job.
+I would like to improve this by adding some copy confirmation and a progress status.
+Probably also create actual functions that will perfom these operations.
+'''
+username = input(str('Provide a username: '))
+password = input(str('And a password: '))
+
+# src = os.getcwd()
+src = input('Provide a source dir: ')
+#src_files = os.listdir(src)
+# src = r'Your source goes here'
+pattern = r'[\w]{2}-[\w]{3}-[\w]{3}-[\d]\w*'
+d_sl = r'\\'
+dest_fin = r'c$\ticker\in'
+
+for file_name in os.listdir(src):
+    result = re.search(pattern, file_name)
+    dest_srv = result.group(0)
+    full_file_name = os.path.join(src, file_name)
+    dest = os.path.join(d_sl, dest_srv, dest_fin)
+    if os.path.isfile(full_file_name):
+        dest = os.path.join(d_sl, dest_srv, dest_fin)
+        os.system(f"net use {dest} /user:{username} {password} 2>&1>null")
+        shutil.copy(full_file_name, dest)
+        print(f"{file_name} copied to {dest_srv}")
+        os.system("net use * /delete /y 2>&1>null")
