621e33194ff2c756b5a9341d9c0407a283592716
tensoon <son1k@protonmail.com>
2019-12-09 13:06:59 +0000

    adding more files

---
+++ circle_area.py
@@ -0,0 +1,12 @@
+from math import pi
+
+
+def circle_area(r):
+    if type(r) not in [int, float]:
+        raise TypeError("The radius must be a non-negative real number.")
+    if r < 0:
+        raise ValueError("The value cannot be negative.")
+    return pi*(r**2)
+
+
+print(circle_area(4))
---
+++ fibs.py
@@ -0,0 +1,31 @@
+from functools import lru_cache
+
+"""fib_cache = {}
+
+def fib(n):
+    if n in fib_cache:
+        return fib_cache[n]
+
+    if n == 1:
+        value = 1
+    elif n == 2:
+        value =1
+    elif n > 2:
+        value = fib(n-1) + fib(n-2)
+    fib_cache[n] = value
+    return value
+
+for n in range(1, 10000):
+    print(f"{n} : {fib(n)}")"""
+
+@lru_cache(maxsize=100000)
+def fib2(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    elif n > 2:
+        return fib2(n-1) + fib2(n-2)
+
+for n in range(1, 2001):
+    print(f"{n} : {fib2(n)}")
