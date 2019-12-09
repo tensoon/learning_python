0c4729bdc1cb425b7b0246f1d67e043f3d6da5f1
tensoon <son1k@protonmail.com>
2019-12-09 12:38:15 +0000

    Adding comments to ggg.py

--- ggg.py
+++ ggg.py
@@ -1,19 +1,22 @@
 from ftplib import FTP
 
+# Connect and list contents.
 ftp = FTP(host='url goes here')
 ftp.login(user='username', passwd='password')
 list_cont = ftp.nlst()
 
-substr = 'ExchangeTrading'
-exchange_trading = list(filter(lambda s: substr in s, list_cont))
-exchange_trading.sort()
-file_to_get = exchange_trading[-1]
+# The below will create a list form list_cont only containing the items we are concerned with.
+# This is so that we can then sort the new list and always download the most recent item.
+substr = 'part of filename here'
+final_list = list(filter(lambda s: substr in s, list_cont))
+final_list.sort()
+file_to_get = final_list[-1]
 
-
-def grabFile(fname):
+# Download happens here:
+def grab_file(fname):
     print(f"Downloading {fname}")
     ftp.retrbinary('RETR ' + fname, open(fname, 'wb').write, 2400)
     print(f"Successfully downloaded {fname}")
     ftp.quit()
 
-grabFile(file_to_get)
+grab_file(file_to_get)
