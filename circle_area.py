c68f57ccc8b98d7aa8105daff29f5d65edc2120e
son1k@protonmail.com <son1k@protonmail.com>
2019-12-22 11:55:17 +0000

    playing around with formatting

--- circle_area.py
+++ circle_area.py
@@ -6,7 +6,7 @@ def circle_area(r):
         raise TypeError("The radius must be a non-negative real number.")
     if r < 0:
         raise ValueError("The value cannot be negative.")
-    return pi*(r**2)
+    return pi * (r ** 2)
 
 
 print(circle_area(4))
--- csv_json.py
+++ csv_json.py
@@ -1,12 +1,14 @@
 import csv
 import json
 
+
 def csvtojson(src, dst):
     with open(src) as f:
         reader = csv.DictReader(f)
         rows = list(reader)
 
-    with open(dst, 'w') as f:
+    with open(dst, "w") as f:
         json.dump(rows, f)
 
-csvtojson('test.csv', 'test.file.json')
+csvtojson("test.csv", "test.file.json")
+
