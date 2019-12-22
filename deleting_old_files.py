15db851642196fa5121ca703fae2e5eafa30536c
son1k@protonmail.com <son1k@protonmail.com>
2019-12-22 11:38:26 +0000

    reformatting

--- deleting_old_files.py
+++ deleting_old_files.py
@@ -8,9 +8,9 @@ now = time.time()
 only_files = []
 
 for file in os.listdir(folder_path):
-    file_full_path = os.path.join(folder_path,file)
-    if os.path.isfile(file_full_path): #and file.endswith(file_ends_with):
-        #Delete files older than x days
-        if os.stat(file_full_path).st_mtime < now - days * 86400: 
-             os.remove(file_full_path)
-             print ("File Removed: " , file_full_path)
+    file_full_path = os.path.join(folder_path, file)
+    if os.path.isfile(file_full_path):  # and file.endswith(file_ends_with):
+        # Delete files older than x days
+        if os.stat(file_full_path).st_mtime < now - days * 86400:
+            os.remove(file_full_path)
+            print("File Removed: ", file_full_path)
--- dfg.py
+++ dfg.py
@@ -1,12 +1,27 @@
-ftp_dir_list = ['CentreChangeReport_20190919.txt', 'CentreChangeReport_20191114.txt', 'ExchangeSettlement_20190919.txt', \
-     'ExchangeSettlement_20191003.txt', 'ExchangeSettlement_20191017.txt', 'ExchangeSettlement_20191031.txt', \
-         'ExchangeSettlement_20191114.txt', 'ExchangeTrading_20200106.txt', 'ExchangeSettlement_20191128.txt', 'ExchangeTrading_20190919.txt', \
-             'ExchangeTrading_20191003.txt', 'ExchangeTrading_20191017.txt', 'ExchangeTrading_20191031.txt', \
-                 'ExchangeTrading_20191114.txt', 'ExchangeTrading_20191128.txt', 'ExchangeTrading_20191206.txt', 'THR_20190920.csv.zip', \
-                     'THR_20191018.csv.zip', 'THR_20191122.csv.zip']
+ftp_dir_list = [
+    "CentreChangeReport_20190919.txt",
+    "CentreChangeReport_20191114.txt",
+    "ExchangeSettlement_20190919.txt",
+    "ExchangeSettlement_20191003.txt",
+    "ExchangeSettlement_20191017.txt",
+    "ExchangeSettlement_20191031.txt",
+    "ExchangeSettlement_20191114.txt",
+    "ExchangeTrading_20200106.txt",
+    "ExchangeSettlement_20191128.txt",
+    "ExchangeTrading_20190919.txt",
+    "ExchangeTrading_20191003.txt",
+    "ExchangeTrading_20191017.txt",
+    "ExchangeTrading_20191031.txt",
+    "ExchangeTrading_20191114.txt",
+    "ExchangeTrading_20191128.txt",
+    "ExchangeTrading_20191206.txt",
+    "THR_20190920.csv.zip",
+    "THR_20191018.csv.zip",
+    "THR_20191122.csv.zip",
+]
 
-#print(ftp_dir_list)
-substr = 'ExchangeTrading'
+# print(ftp_dir_list)
+substr = "ExchangeTrading"
 res = list(filter(lambda s: substr in s, ftp_dir_list))
 
 res.sort()
