c43c11e1294cfaba701d0d2addd0fd6b10481bde
tensoon <son1k@protonmail.com>
2020-01-23 12:46:31 +0000

    adding comments

--- track_stuff.py
+++ track_stuff.py
@@ -19,13 +19,13 @@ def check_price():
     replaced_str = strip_price.replace(",", ".")
     final_price = float(replaced_str)
 
-    if final_price <= 1.249:
+    if final_price <= 1.200:
         print("The price target has been hit!\nSending email now.")
         send_email()
     else:
         print("Price has not reached the desired level.\nWill check again in 12 hours.")
 
-
+# Create an instance of an SMTP server to facilitate the sending of emails
 def send_email():
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.ehlo()
@@ -33,18 +33,18 @@ def send_email():
     server.ehlo()
 
     server.login("balinski.n@gmail.com", "password")
-
+# Creates the subject and body of the email
     subject = "PRICE ALERT!!! - VGA GeForce RTX 2080 Ti FTW3 ULTRA GAMING"
     body = "Follow the link: https://www.amazon.co.uk/dp/B07K6H583G/?coliid=ILF3LQY0QDM0S&colid=1JTQ8MQFGDJJI&psc=1&ref_=lv_ov_lig_dp_it"
     msg = f"Subject: {subject}\n\n{body}"
 
     server.sendmail("balinski.n@gmail.com", "son1k@protonmail.com", msg)
-
+# Kill the server.
     server.quit()
-
 
 check_price()
 
+# Can run as a background process or schedule through cron.
 """while True:
     check_price()
     time.sleep(43200)"""
